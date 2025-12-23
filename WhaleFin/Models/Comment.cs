3dd29bd8f693d7e4c4ad95d9ac119e9e640ffe85
AnmolChy999 <anmol.cha6644@gmail.com>
2025-12-23 09:22:10 +0545

    Initial Commit

---
+++ WhaleFin/Models/Comment.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhaleFin.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.UtcNow;
+
+        public int? StockId { get; set; }
+
+        public Stock? Stock { get; set; }
+        
+    }
+}
